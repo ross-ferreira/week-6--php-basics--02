f5629070f3f7894fa0a0b28a7c176af310d6de7c
Ross Ferreira <ross.p.ferreira@gmail.com>
2020-02-10 16:11:42 +0000

    added challenge2.2

---
+++ challenge2.2.php
@@ -0,0 +1,13 @@
+<?php
+
+declare (strict_types=1);
+
+
+function hello(string $name) :string {
+    return "Hello {$name}";
+};
+
+var_dump(hello("alice")); // string(11) "hello alice"
+var_dump(hello("bob")); // string(9) "hello bob"
+
+?> 
